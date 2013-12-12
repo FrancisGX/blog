178ee99a101cee0e1dc4b5012b49666ff68cd152
Frank Fuston <Frank.fuston@gmail.com>
2013-12-12 08:24:50 -0600

    Remove the Zombie class examle.
    - If I keep the example I will either have to explain .each or
      ActiveRecord::Base and honestly I just don't want to do either.
    
    - Other small changes.

--- improve_your_code_with_collection_closure_methods.rb
+++ improve_your_code_with_collection_closure_methods.rb
@@ -2,7 +2,7 @@
 
 # When working with collections of data we often need to iterate over (or traverse)
 # one collection, do something with each element, and gather the results. You might
-# find that the code below looks familiar.
+# find the code below familiar.
 
 zombie_names = []
 
@@ -10,21 +10,24 @@ zombies.each do |zombie|
   zombie_names << zombie.name
 end
 
-# Here we want all of the names of the zombies so we iterate over our collection
-# of zombies and add each name to the zombie_names array.
+# Here we want all of the zombies names so we create an empty array
+# and assign it to the variable 'zombie_names'
+# Assuming that we already have a collection of zombies, we iterate
+# over them adding each zombie's name to the zombie_names array.
 
 # Code patterns similar to this one are so common that Ruby actually has built-in
 # support to help you acheive the same effect with less code. Check out Enumerable.
 # (http://ruby-doc.org/core-1.9.3/Enumerable.html) One example of a method
-# Enumerable gives us is map. (http://ruby-doc.org/core-1.9.3/Enumerable.html#method-i-map)
+# Enumerable gives us is .map. (http://ruby-doc.org/core-1.9.3/Enumerable.html#method-i-map)
 # Map takes a block and returns a new array containing the results of calling that block
-# once for every element in the original collection. Let's look at an example.
+# once for every element in the collection. Let's look at an example.
 
 collection = [1, 2, 3]
 
 collection.map do |element|
   element * 5
 end
+
 # => [5, 10, 15]
 
 # Now how does this differ from the .each method? As we saw above .map returns the
@@ -71,9 +74,8 @@ zombies.each do |zombie|
   brainless << zombie if zombie.brainless?
 end
 
-# Here we create an empty array and assign it to the variable 'brainless'
-# Assuming that we already have a collection of zombies, we iterate
-# over them adding each one to the brainless array if it is brainless.
+# This time we iterate over zombies adding each one to the brainless
+# array if it is brainless.
 # Again, so common that Enumerable gives us a method to do just that.
 
 brainless = zombies.select { |zombie| zombie.brainless? }
@@ -105,7 +107,7 @@ zombies.each do |zombie|
   limbs += zombie.limbs
 end
 
-# But Enumerable but of course Enumerable has a handy .inject method.
+# But of course Enumerable has a handy .inject method.
 
 limbs = zombies.inject(0) { |sum, zombie| sum + zombie.limbs }
 
@@ -113,26 +115,3 @@ limbs = zombies.inject(0) { |sum, zombie| sum + zombie.limbs }
 # all you need to know to get started using it is that the parameter
 # you pass it will be used as the starting value and will be passed
 # to the block as the first block parameter.
-
-
-# Example:
-
-class Zombie
-  attr_accessor :name, :brain, :limbs
-
-  def initialize(name, brain, limbs)
-    @name = name
-    @brain = brain
-    @limbs = limbs
-  end
-
-  def brainless?
-    !brain
-  end
-end
-
-zombies = [
-  Zombie.new('Bill', true, 4),
-  Zombie.new('Tim', true, 3),
-  Zombie.new('Phil', false, 2)
-]
